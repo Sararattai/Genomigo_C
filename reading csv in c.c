841dacea942a3d7d91c2d1878435f0678025e99c
Sararattai <39612325+Sararattai@users.noreply.github.com>
2018-06-01 19:36:57 +0530

    read in c.c

---
+++ reading csv in c.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+file = fileptr;
+fptr = fopen("users.csv","r");
+if(file == NULL)
+{
+printf("error");
+return 1;
+}
+while(!foo (fptr))
+{
+ fgets(line,fptr);
+ puts(line);
+ }
+ fclose(fptr);
+ return 0;
+ }
